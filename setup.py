eb13613e75e9fbc5cd0796b31ed9b363d8bcc226
Nagy Krisztián <knagy@deadlime.hu>
2018-02-23 21:07:59 +0100

    bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='mitmproxy-escher',
     description='Sign mitmproxy requests with Escher',
-    version='0.1.3',
+    version='1.0.0',
     url='https://github.com/knagy/mitmproxy-escher',
     author='Nagy Krisztián',
     author_email='knagy@deadlime.hu',
